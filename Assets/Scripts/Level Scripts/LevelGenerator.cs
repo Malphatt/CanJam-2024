6c6497e6c44486cc90863e3671692c5554f7a738
MLowen1 <mikelowenuk@gmail.com>
2024-11-23 13:32:21 +0000

    Finished Pathfinding

--- Assets/Scripts/Level Scripts/LevelGenerator.cs
+++ Assets/Scripts/Level Scripts/LevelGenerator.cs
@@ -1,13 +1,11 @@
-// Ignore Spelling: Pcg
-
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
 using UnityEditor;
 using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour
 {
-
     // 2D array to store procedural generation (PCG) points.
     private PcgPoints[,] pointsArray;
 
@@ -22,6 +20,14 @@ public class LevelGenerator : MonoBehaviour
     [SerializeField]
     [Tooltip("The height of the level")]
     private int gridHeight = 10;
+
+    private PcgPoints currentPoint;
+
+    // Room Settings
+    [Header("Room Settings")]
+    [SerializeField]
+    //Number of rooms to generate
+    private int numberOfRooms = 10;
 
     // Room prefabs.
     [Header("Room Prefabs")]
@@ -42,8 +48,6 @@ public class LevelGenerator : MonoBehaviour
 
         SetStartPoint();
 
-        CheckNeighbours();
-
         CreateRooms();
     }
 
@@ -52,7 +56,7 @@ public class LevelGenerator : MonoBehaviour
         // Initialize the points array.
         pointsArray = new PcgPoints[gridWidth, gridHeight];
 
-        //Initialize the available neighbours array
+        // Initialize the available neighbours array
         availableNeighbours = new PcgPoints[4];
 
         // Iterate through the grid and create a point for each position.
@@ -61,96 +65,13 @@ public class LevelGenerator : MonoBehaviour
             for (int y = 0; y < gridHeight; y++)
             {
                 // Create a data point for each grid position, with each point 10 units apart.
-                Vector3 point = new Vector3(x * 10, 0, y * 10);
+                Vector3 point = new Vector3(x * 100, 0, y * 100);
 
                 // Initialize the PCG point.
-                PcgPoints newPoint = new PcgPoints(point);
+                PcgPoints newPoint = new PcgPoints(point, x, y);
 
                 // Store the point in the array.
                 pointsArray[x, y] = newPoint;
-            }
-        }
-    }
-
-    void CheckNeighbours()
-    {
-        for (int x = 0; x < gridWidth; x++)
-        {
-            for (int y = 0; y < gridHeight; y++)
-            {
-                PcgPoints point = pointsArray[x, y];
-
-                // Check if this is the current point.
-                if (point.isCurrentPoint)
-                {
-                    // Check if the point has a neighbour to the right.
-                    if (x < gridWidth - 1)
-                    {
-                        PcgPoints rightNeighbour = pointsArray[x + 1, y];
-                        // Check if the right neighbour is not blocked.
-                        if (!rightNeighbour.isBlocked)
-                        {
-                            // Instantiate room 2 at the right neighbour's position.
-                            //GameObject room = Instantiate(Room2, rightNeighbour.point, Quaternion.identity);
-
-                            //add the right neighbour to the available neighbours array
-                            availableNeighbours[0] = rightNeighbour;
-                        }
-                    }
-
-                    // Check if the point has a neighbour to the left.
-                    if (x > 0)
-                    {
-                        PcgPoints leftNeighbour = pointsArray[x - 1, y];
-
-                        if (!leftNeighbour.isBlocked)
-                        {
-                            // Instantiate room 2 at the right neighbour's position.
-                            //GameObject room = Instantiate(Room2, leftNeighbour.point, Quaternion.identity);
-
-                            //add the left neighbour to the available neighbours array
-                            availableNeighbours[1] = leftNeighbour;
-                        }
-                    }
-
-                    // Check if the point has a neighbour above.
-                    if (y < gridHeight - 1)
-                    {
-                        PcgPoints topNeighbour = pointsArray[x, y + 1];
-
-                        if (!topNeighbour.isBlocked)
-                        {
-                            // Instantiate room 2 at the right neighbour's position.
-                            //GameObject room = Instantiate(Room2, topNeighbour.point, Quaternion.identity);
-
-                            //add the top neighbour to the available neighbours array
-                            availableNeighbours[2] = topNeighbour;
-                        }
-                    }
-
-                    // Check if the point has a neighbour below.
-                    if (y > 0)
-                    {
-                        PcgPoints bottomNeighbour = pointsArray[x, y - 1];
-
-                        if (!bottomNeighbour.isBlocked)
-                        {
-                            // Instantiate room 2 at the right neighbour's position.
-                            //GameObject room = Instantiate(Room2, bottomNeighbour.point, Quaternion.identity);
-
-                            //add the bottom neighbour to the available neighbours array
-                            availableNeighbours[3] = bottomNeighbour;
-                        }
-                    }
-
-                    //set the current point to blocked
-                    point.isBlocked = true;
-                    //set the current point to not current
-                    point.isCurrentPoint = false;
-
-                    // Exit the loops once the current point is found and its neighbours are checked.
-                    return;
-                }
             }
         }
     }
@@ -170,7 +91,6 @@ public class LevelGenerator : MonoBehaviour
             }
         }
 
-
         // Select the center point of the grid
         int centerPointX = gridWidth / 2;
         int centerPointY = gridHeight / 2;
@@ -179,54 +99,76 @@ public class LevelGenerator : MonoBehaviour
         GameObject room = Instantiate(Room1, pointsArray[centerPointX, centerPointY].point, Quaternion.identity);
 
         //set the current point to true
-        pointsArray[centerPointX, centerPointY].isCurrentPoint = true;
+        currentPoint = pointsArray[centerPointX, centerPointY];
+        pointsArray[centerPointX, centerPointY].isBlocked = true;
 
-        
+    }
+
+    List<PcgPoints> GetNeighbours(PcgPoints point)
+    {
+        List<PcgPoints> neighbours = new List<PcgPoints>();
+
+        Vector2[] directions = new Vector2[4]
+        {
+            new Vector2(1, 0),
+            new Vector2(-1, 0),
+            new Vector2(0, 1),
+            new Vector2(0, -1)
+        };
+
+        for (int i = 0; i < directions.Length; i++)
+        {
+            Vector2 dir = directions[i];
+            Vector2 neighbourPos = new Vector2(point.X + dir.x, point.Y + dir.y);
+
+            if (neighbourPos.x >= 0 && neighbourPos.x < gridWidth && neighbourPos.y >= 0 && neighbourPos.y < gridHeight)
+            {
+                PcgPoints neighbour = pointsArray[(int)neighbourPos.x, (int)neighbourPos.y];
+
+                if (!neighbour.isBlocked)
+                {
+                    neighbours.Add(neighbour);
+                }
+            }
+        }
+
+        return neighbours;
     }
 
     void CreateRooms()
     {
-
-            Debug.Log("Creating rooms");
+        // run these two functions in a loop until the number of rooms is reached
+        for (int i = 0; i < numberOfRooms; i++)
+        {
+            List<PcgPoints> neighbours = GetNeighbours(currentPoint);
 
             //select a random neighbour from the available neighbours array
-            PcgPoints randomNeighbour = availableNeighbours[Random.Range(0, availableNeighbours.Length)];
+            PcgPoints randomNeighbour = neighbours[Random.Range(0, neighbours.Count)];
 
             //create a room at the random neighbour's position
             GameObject room = Instantiate(Room1, randomNeighbour.point, Quaternion.identity);
 
-            //set the current point to true
-            Debug.Log("Current point is " + randomNeighbour.point + "isCurrentPoint" + randomNeighbour.isCurrentPoint);
-
-            randomNeighbour.isCurrentPoint = true;
-            Debug.Log("Current point is " + randomNeighbour.point + "isCurrentPoint" + randomNeighbour.isCurrentPoint);
+            //set the current point
+            currentPoint = randomNeighbour;
 
             //set all of the available neighbours to blocked
-            for (int i = 0; i < availableNeighbours.Length; i++)
-            {
-                availableNeighbours[i].isBlocked = true;
-            }
-
-            for (int i = 0; i < availableNeighbours.Length; i++)
-        {
-            Debug.Log("Available neighbour " + i + " is " + availableNeighbours[i].point + "isBlocked" + availableNeighbours[i].isBlocked);
+            for (int j = 0; j < neighbours.Count; j++)
+                pointsArray[neighbours[j].X, neighbours[j].Y].isBlocked = true;
         }
-
-        //remove all elements of the available neighbours array
-        availableNeighbours = new PcgPoints[4];
-
     }
 
     struct PcgPoints
     {
+        public readonly int X;
+        public readonly int Y;
         public readonly Vector3 point;
-        public bool isCurrentPoint;
         public bool isBlocked;
 
-        public PcgPoints(Vector3 _point) 
+        public PcgPoints(Vector3 _point, int _x, int _y)
         {
+            this.X = _x;
+            this.Y = _y;
             point = _point;
-            isCurrentPoint = false;
             isBlocked = false;
         }
     }
