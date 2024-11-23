2bd22702b7eb6e928cb4ef4b489068d02f30c12b
Malphatt <Ocelot.mf@gmail.com>
2024-11-23 18:32:11 +0000

    Flipside Animation

---
+++ Assets/Scripts/Enemies/Enemy States/EnemyState_Cover.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyState_Cover : IState
+{
+    private NavMeshAgent _navMeshAgent;
+
+    public void OnEnter()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnExit()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Tick()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void TickFixed()
+    {
+        throw new System.NotImplementedException();
+    }
+}
